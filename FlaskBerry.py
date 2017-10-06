75c5adbc945be334cb16aca0caf0326a5739d676
apheyhys <apheyhys@gmail.com>
2017-10-06 21:23:29 +0300

    new

--- FlaskBerry.py
+++ FlaskBerry.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_phrase = phrase, the_letters = letters, the_results = results, the_title = title,)
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title = 'Welcome to search4letters on the web!')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
