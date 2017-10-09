7aec5f9377ca43519cea5b3fa9af9306bb6a5150
apheyhys <apheyhys@gmail.com>
2017-10-09 12:04:41 +0300

    new

--- FlaskBerry.py
+++ FlaskBerry.py
@@ -4,13 +4,20 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 
+def log_request(req, res):
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_phrase = phrase, the_letters = letters, the_results = results, the_title = title,)
+
 
 @app.route('/')
 @app.route('/entry')
