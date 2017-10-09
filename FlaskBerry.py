57c279a61cfadbd85c2fdfc9289cccf7050e0fbd
apheyhys <apheyhys@gmail.com>
2017-10-09 17:13:43 +0300

    new

--- FlaskBerry.py
+++ FlaskBerry.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -10,7 +15,9 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_phrase = phrase, the_letters = letters, the_results = results, the_title = title,)
+
 
 @app.route('/')
 @app.route('/entry')
@@ -18,5 +25,18 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title = 'Welcome to search4letters on the web!')
 
 
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for chore in log:
+            contents.append([])
+            for item in chore.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title = 'View Log', the_row_titles = titles, the_data = contents,)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
